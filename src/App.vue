c3c5106b079285e40b7bb5d301e8ba61288b5bcb
Songsong <614750803@qq.com>
2022-03-14 21:41:06 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Frank from './components/Frank.vue'
+//创建一个组件
+const App = {
+  // 在组件中使用 <script setup>
+  components: {
+    HelloWorld,
+    Frank
+  },
+  // 在组件中使用 <template>
+  template: `
+    <div>123
+      <h1>Hello World</h1>
+      <HelloWorld />
+      <Frank />
+    </div>
+  `
+}
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Frank  />
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Frank.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+</template>
