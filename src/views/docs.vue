bb95edcb2ddcdd44c0170a4c589e37eb3a01502e
Songsong <614750803@qq.com>
2022-03-15 23:28:34 +0800

    侧边栏aside

--- src/views/docs.vue
+++ src/views/docs.vue
@@ -1,7 +1,24 @@
 <template>
     <Topnav/>
     <div class="content">
-        <aside>边</aside>
+       <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/swich">Swich 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
+            
         <main>主要</main>
     </div>
 </template>
