6333b1e3b089885bf332b723dc279b657f8e5c12
Songsong <614750803@qq.com>
2022-03-19 17:28:23 +0800

    添加dialog css

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>dialog</div>
-</template>
-<script lang="ts">
-export default{
 
-}
+<Dialog> 123</Dialog>
+
+</template>
+<script lang="ts" setup>
+import Dialog from "../lib/dialog1.vue"
 
 </script>
