188088bca655e9980ea55eb40decb8d2bce830bc
Songsong <614750803@qq.com>
2022-03-19 15:58:44 +0800

    size css

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,8 +1,16 @@
 <template>
     <Button  >nihao</Button>
-    <Button  theme="button">nihaonihaonihaonihaoni haonihaon haonihaon haonihaon haonihaonihaonihao</Button>
+    <Button   theme="button">nihaonihaonihaonihaoni haonihaon haonihaon haonihaon haonihaonihaonihao</Button>
     <Button  theme="link">nihao</Button>
     <Button  theme="a">nihao</Button>
+
+    <hr>
+    <h1>示例2</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
 </template>
 <script lang="ts" setup>
 import Button from '../lib/button1.vue'
--- src/lib/button1.vue
+++ src/lib/button1.vue
@@ -1,16 +1,29 @@
 <template>
-    <button class="gulu-button" :class="`gulu-theme-${theme}`">
+    <button class="gulu-button" :class="classes">
         <slot></slot>
     </button>
 
 </template>
 <script lang="ts" setup>
+import { computed } from "@vue/runtime-core"
+
 const props = defineProps({
     theme:{
         type:String,
         default:'button'
+    },
+    size:{
+        type:String,
+        default:'normal'
     }
 })
+    const {theme,size} = props
+     const classes = computed(() => {
+        return {
+            [`gulu-theme-${theme}`]:theme,
+        [`gulu-size-${size}`]:size
+      }
+    })
 </script>
 
 <style lang="scss" >
@@ -47,5 +60,36 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.gulu-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.gulu-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.gulu-theme-button{
+    &.gulu-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+    &.gulu-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+  }
+
 }
 </style>
