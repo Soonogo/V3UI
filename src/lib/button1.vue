0ea91eb4d8cbd457d352d394d79c40461230949d
Songsong <614750803@qq.com>
2022-03-19 17:04:20 +0800

    完成button组件

--- src/lib/button1.vue
+++ src/lib/button1.vue
@@ -1,5 +1,6 @@
 <template>
-    <button class="gulu-button" :class="classes">
+    <button class="gulu-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="gulu-loadingIndicator"></span>
         <slot></slot>
     </button>
 
@@ -15,13 +16,27 @@ const props = defineProps({
     size:{
         type:String,
         default:'normal'
+    },
+    level:{
+        type:String,
+        default:'main'
+    },
+    disabled:{
+        type:Boolean,
+        default:false
+    },
+    loading:{
+        type:Boolean,
+        default:false
     }
+    
 })
-    const {theme,size} = props
+    const {theme,size,level} = props
      const classes = computed(() => {
         return {
             [`gulu-theme-${theme}`]:theme,
-        [`gulu-size-${size}`]:size
+        [`gulu-size-${size}`]:size,
+                [`gulu-level-${level}`]: level,
       }
     })
 </script>
@@ -32,6 +47,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: #f5222d;
+$grey:grey; 
 .gulu-button {
   box-sizing: border-box;
   height: $h;
@@ -89,6 +106,84 @@ $radius: 4px;
       height: 20px;
       padding: 0 4px;
     }
+    &.gulu-theme-button {
+    &.gulu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.gulu-theme-link {
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.gulu-theme-text {
+    &.gulu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+   &.gulu-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.gulu-theme-link, &.gulu-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+   > .gulu-loadingIndicator{
+       border: 1px solid red;
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px; 
+    border-color: white white white transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: gulu-spin 1s infinite linear;
+  }
+}
+@keyframes gulu-spin {
+  0%{transform: rotate(0deg)} 
+  100%{transform: rotate(360deg)} 
   }
 
 }
--- src/lib/switch1.vue
+++ src/lib/switch1.vue
@@ -19,34 +19,19 @@
   $h: 22px;
   $h2: $h - 4px;
   .gulu-switch {
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: grey;
-    border-radius: 11px;
-    position: relative;
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  > span {
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
-  span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:white;
-    border-radius: 11px;
-    transition: all 250ms;
-
-  } 
-  button.gulu-checked{
-    background: blue;
+  &.gulu-checked { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
   }
-   button.gulu-checked > span {
-    left: calc(100% - #{$h2} - 2px);
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
   }
-   button:active{
-    > span {width: $h2 + 4px;}
+  &.gulu-checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
   }
-  button.gulu-checked:active{
-    > span {width: $h2 + 4px; margin-left: -4px;}
-  }
+}
 </style>
