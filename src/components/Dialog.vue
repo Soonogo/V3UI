12b402726846acafe6627f98381bbc7a74d0746f
Songsong <614750803@qq.com>
2022-03-20 23:15:51 +0800

    快速添加Dialog函数openDialog

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -8,14 +8,22 @@
         <div>事了拂衣去</div>
         <div>深藏身与名</div>
     </template>
-    
 </Dialog>
+<h1>发呢和</h1>
+<Button @click="showDialog">3333</Button>
 
 </template>
 <script lang="ts" setup>
 import { ref } from "vue"
 import Dialog from "../lib/dialog1.vue"
 import Button from "../lib/button1.vue"
+import openDialog from "../lib/openDialog"
+const showDialog=()=>{
+    openDialog({
+        title:"白哦",
+        content:"今夕何夕,见此良人",
+    })
+}
 const v = ref(false)
 const x = ()=>{
     v.value = !v.value
---
+++ src/lib/openDialog.ts
@@ -0,0 +1,27 @@
+import { createApp,h } from 'vue';
+import Dialog from "./dialog1.vue";
+const openDialog = (options)=>{
+    const {title,content,ok ,cancel} = options;
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const close = ()=>{
+        app.unmount(div)
+        div.remove()
+    }
+   const app =  createApp({
+        render(){
+            return h(Dialog,{
+                visiable:true,
+                "onUpdate:visiable":(e)=>{
+                    if(e===false){
+                        close()
+                    }
+                },ok,cancel
+            },{title,content}
+            )   
+        }
+    })
+    app.mount(div);
+}
+
+export default openDialog;
