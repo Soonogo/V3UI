2d067980ac0dd080a65087ebd75ca0d1779c0689
Songsong <614750803@qq.com>
2022-03-23 21:50:34 +0800

    创建tab和tab1

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,3 +1,12 @@
 <template>
-    <div>tabs</div>
+    <Tabs>
+        <Tab title="标题1">1</Tab>
+        <Tab title="标题1">2</Tab>
+        <Tab title="标题1">3</Tab>
+    </Tabs>
 </template>
+<script lang="ts" setup>
+import Tabs from "../lib/tabs1.vue"
+import Tab from "../lib/tab.vue"
+
+</script>
