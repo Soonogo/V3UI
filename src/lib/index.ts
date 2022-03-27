e538bf4c38b1e048f404c99f793bce0a78290523
Songsong <614750803@qq.com>
2022-03-27 21:52:08 +0800

    更新packjson

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export { default as Switch } from './switch1.vue';
+export { default as Button } from './button1.vue';
+export { default as Tabs } from './tabs.vue';
+export { default as Tab } from './tab.vue';
+export { default as Dialog } from './dialog1.vue';
+export { openDialog as openDialog } from './openDialog';
